3f7926a23c23e27c473de80dc902b8e9473d21a9
JuanAparicioFalcon <juanapariciofalcon@gmail.com>
2025-02-12 08:50:39 +0100

    codigo plantilla y correcion de rutas de las imagenes

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -4,6 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [  provideAnimations(), provideLottieOptions({ player }),,provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
+
+
+
+
+
+
+
+
+
+
